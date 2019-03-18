b08d3f6f4e72ab6212602a09fb85c3857963266c
Linas Martusevicius <linas.martusevicius@gmail.com>
2019-03-18 16:18:34 +0200

    Added persist and sensitive settings gitignore.
    Setting up for hoggit devs.

--- AWACS/app.js
+++ AWACS/app.js
@@ -1,3 +1,63 @@
 'use strict';
 
-console.log('Hello world');
+const _ = require('lodash');
+
+const { Client,RichEmbed } = require('discord.js');
+const client = new Client();
+
+var settings = require("./common/awacs-settings.json");
+var versions = require("./dataaccess/version-checker.js");
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    setInterval(processVersionsInternal,300*1000);
+});
+
+client.login(settings.token);
+
+function processVersionsInternal() {
+
+    var callback = function(stableVersion, openbetaVersion) {
+
+        console.log("stable = "+stableVersion);
+        console.log("openbeta = " + openbetaVersion);
+
+        if (stableVersion !== null) {
+
+            _.each(settings.notificationChannelIds,
+                function(channelId) {
+                    var channel = client.channels.get(channelId);
+                    if (channel !== undefined) {
+                        var embed = new RichEmbed()
+                            .setTitle("DCS World Updated")
+                            .setColor(0x0000FF)
+                            .setURL("https://forums.eagle.ru/showthread.php?t=214251")
+                            .setDescription("DCS World Stable branch updated to " + stableVersion);
+                        channel.send(embed);
+                    }
+                });
+            //stableVersion changed
+            //send message to notification channel
+        }
+        if (openbetaVersion !== null) {
+            _.each(settings.notificationChannelIds,
+                function(channelId) {
+                    var channel = client.channels.get(channelId);
+                    if (channel !== undefined) {
+                        var embed = new RichEmbed()
+                            .setTitle("DCS World Updated")
+                            .setColor(0xFFFF00)
+                            .setURL("https://forums.eagle.ru/showthread.php?p=3834480")
+                        .setDescription("DCS World OpenBeta branch updated to " + openbetaVersion);
+                        channel.send(embed);
+                    }
+                });
+            //openbetaVersion changed
+            //send message to notification channel
+        }
+    };
+
+    versions.GetLatestVersions(callback);
+}
+//wait 60s before exit
+//setTimeout(function() { console.log("DONE"); }, 60000);
---
+++ AWACS/dataaccess/version-checker.js
@@ -0,0 +1,74 @@
+﻿// version-checker.js
+// =================
+const storage = require('node-persist');
+const cheerio = require("cheerio");
+const request = require("request");
+var exec = require('child_process').exec;
+var path = require('path');
+
+const settings = require("../common/awacs-settings.json");
+
+
+initStorage();
+
+let stableVersionFromSite = null;
+let openbetaVersionFromSite = null;
+var latestVersionCallback = null;
+
+module.exports = {
+    GetLatestVersions: async function(callback) {
+
+        latestVersionCallback = callback;
+
+        request({
+                uri: settings.dcsVersionEndpoint,
+            },await requestCallback);
+    }
+};
+
+async function initStorage() {
+    var parentDir = path.resolve(process.cwd());
+    await storage.init({
+        dir: parentDir+"/persist/"
+    });
+}
+
+async function requestCallback(error, response, body) {
+    if (error === null && response.statusCode===200 && body!==null) {
+
+        var site = cheerio.load(body);
+
+        var stableFromSite = site("h2:contains('stable')");
+        var openbetaFromSite = site("h2:contains('openbeta')");
+
+        var stableFromSiteSplit = stableFromSite.html().split(' ').pop();
+        var openbetaFromSiteSplit = openbetaFromSite.html().split(' ').pop();
+
+//        console.log(stableFromSiteSplit);
+//        console.log(openbetaFromSiteSplit);
+
+        var lastStableFromStorage = await storage.getItem("lastStableVersion");
+        var lastOpenbetaFromStorage = await storage.getItem("lastOpenbetaVersion");
+
+        if (lastStableFromStorage === undefined || lastStableFromStorage !== stableFromSiteSplit) {
+
+            await storage.setItem("lastStableVersion", stableFromSiteSplit);
+            stableVersionFromSite = stableFromSiteSplit;
+            console.log("New stable version: "+stableFromSiteSplit);
+        }
+        if (lastOpenbetaFromStorage === undefined || lastOpenbetaFromStorage !== openbetaFromSiteSplit) {
+
+            await storage.setItem("lastOpenbetaVersion", openbetaFromSiteSplit);
+            openbetaVersionFromSite = openbetaFromSiteSplit;
+            console.log("New OpenBeta version: "+openbetaFromSiteSplit);
+        }
+
+        if (latestVersionCallback && typeof latestVersionCallback==='function') {
+
+            latestVersionCallback(stableVersionFromSite, openbetaVersionFromSite);
+        }
+        
+        stableVersionFromSite = null;
+        openbetaVersionFromSite = null;
+    }
+}
